551814c2f7f6efd13c29b12d40019c5de9176e98
“fredrickUdom” <fredrickudom@gmail.com>
2023-11-23 15:28:01 +0100

    configure my database with config service and install typeorm

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
     imports:[
+        TypeOrmModule.forRootAsync({
+            useFactory:(configService:ConfigService)=> ({
+              type: 'mysql',
+              host: configService.getOrThrow('DB_HOST'),
+              port: configService.getOrThrow('DB_PORT'),
+              username: configService.getOrThrow('DB_USER'),
+              password: configService.getOrThrow('DB_PASSWORD'),
+              database: configService.getOrThrow('DB_NAME'),
+              autoLoadEntities: true,
+              synchronize: configService.getOrThrow('DB_SYYNCHRONIZE'),
+            }),
+            inject:[ConfigService]
+          }),
      
     ]
 })
